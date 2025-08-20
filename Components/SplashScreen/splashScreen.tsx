d2f7b1cb25181b6643fafe48b782b7b85a7894c1
nemanyusifov@gmail.com <nemanyusifov@gmail.com>
2025-08-20 16:51:49 +0400

    created setTimeOut

--- Components/SplashScreen/splashScreen.tsx
+++ Components/SplashScreen/splashScreen.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useRouter } from 'expo-router'
+import React, { useEffect } from 'react'
 import { View } from 'react-native'
 import Loader from '../ui/loader'
 import Logo from '../ui/logo'
 
 const SplashScreen = () => {
+  const router = useRouter()
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      router.replace('/homeScreen')
+    },3000);
+
+    return()=>clearTimeout(timer);
+  },[])
   return (
     <View>
       <Logo/>
